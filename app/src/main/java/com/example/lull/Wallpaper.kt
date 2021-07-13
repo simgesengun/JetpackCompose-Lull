69ece54ed69aca11519771d007de1cef2c3d3976
simgesengun <simgesngn@gmail.com>
2021-07-13 20:57:13 +0300

    Fixed dark theme

--- app/src/main/java/com/example/lull/Wallpaper.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lull
-
-import android.graphics.drawable.Drawable
-
-data class Wallpaper(var id : Int, var name : String, var drawableId : Int, var tags : List<String>, var artist : String? = null) {
-}
